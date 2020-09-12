bf2eab7c478545b711b9e3f7b4381fef0cac75c5
cameronyoung94 <youngcameron0794@gmail.com>
2020-09-11 21:17:42 -0400

    finsihed card component

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,21 @@
+const { default: Axios } = require("axios");
+
+
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-j
+
+axios.get("https://api.github.com/users/cameronyoung94")
+.then((resp) =>{
+console.log('success!', resp.data)  
+document.querySelector(".cards")
+.appendChild(gitCard(resp.data))  
+})
+
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -49,7 +61,60 @@ const followersArray = [];
       </div>
     </div>
 */
+function gitCard (object) {
+  const gitDiv = document.createElement('div')
 
+  const gitImg = document.createElement('img')
+  gitImg.src = object.avatar_url;
+  gitDiv.appendChild(gitImg)
+
+
+  const gitDiv2 = document.createElement('div')
+
+  const gitH3 = document.createElement('h3')
+  gitH3.textContent = object.name;
+  gitDiv2.appendChild(gitH3)
+
+  const gitUser = document.createElement('p')
+  gitUser.textContent = object.login;
+  gitDiv2.appendChild(gitUser)
+
+  const gitLocation = document.createElement('p')
+  gitLocation.textContent = `Location: ${object.location}`;
+  gitDiv2.appendChild(gitLocation)
+  
+  const gitProfile = document.createElement('p')
+  gitProfile.textContent = `Profile: `;
+  gitDiv2.appendChild(gitProfile)
+
+  const gitLink = document.createElement('a')
+  gitLink.textContent = object.html_url;
+  gitProfile.appendChild(gitLink)
+  
+  const gitFollowers = document.createElement('p')
+  gitFollowers.textContent = `Followers: ${object.followers}`
+  gitDiv2.appendChild(gitFollowers)
+
+  const gitFollowing = document.createElement('p')
+  gitFollowing.textContent = `Following: ${object.following}`
+  gitDiv2.appendChild(gitFollowing)
+
+  const gitBio = document.createAttribute('p')
+  gitBio.textContent = `Bio: ${object.bio}`
+  gitDiv2.appendChild(gitBio)
+
+  gitDiv.appendChild(gitDiv2);
+
+  gitDiv.classList.add("card") 
+
+  gitDiv2.classList.add("card-info")
+
+  gitH3.classList.add("name")
+
+  gitUser.classList.add("username")
+
+  return gitDiv
+}
 /*
   List of LS Instructors Github username's:
     tetondan
